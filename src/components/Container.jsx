f3f360da0d66e872650ba6dfeb7f7003d69cc115
aziz-codes <azizisoffline@gmail.com>
2022-12-16 02:52:40 +0500

    usestate and useeffect hooks

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Container = () => {
-  return <div className="text-blue-500">Container</div>;
+  const [value, setValue] = useState(0);
+  const [isDisabled, setisDisabled] = useState(true);
+  useEffect(() => {
+    if (value <= 1) {
+      setisDisabled(true);
+    } else {
+      setisDisabled(false);
+    }
+  }, [value]);
+  return (
+    <div className="flex flex-row gap-2 justify-center">
+      <button
+        onClick={() => {
+          setValue((currentState) => (currentState -= 1));
+        }}
+        disabled={isDisabled}
+      >
+        -
+      </button>
+      <button>{value}</button>
+      <button
+        onClick={() => {
+          setValue((currentState) => (currentState += 1));
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Container;
